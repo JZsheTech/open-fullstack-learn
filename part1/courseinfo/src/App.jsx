a18cb28c9cd84fd0525bd6a1b819dd99fc799bd2
jzshe <1165324684@qq.com>
2025-10-20 21:12:05 +0800

    显示课程列表

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,27 +4,44 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+    <div>
+      <p>
+        {props.parts[0].name} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].name} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercises}
+      </p>
+    </div>
   )
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>
+  return (
+    <p>
+      Number of exercises{' '}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part = 'Fundamentals of React'
-  const exercises = 10
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <h1>{course}</h1>
-      <Content part={part} exercises={exercises} />
-      <Total total={exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }

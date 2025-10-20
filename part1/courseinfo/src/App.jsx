c926cb8730ba0010afecb0788f8d8339bfc80730
jzshe <1165324684@qq.com>
2025-10-20 21:11:42 +0800

    分为Header,Content和Total 3部分显示页面

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,30 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>
+}
+
 const App = () => {
   const course = 'Half Stack application development'
+  const part = 'Fundamentals of React'
   const exercises = 10
 
   return (
     <div>
-      <p>{course}</p>
-      <p>Number of exercises {exercises}</p>
+      <Header course={course} />
+      <h1>{course}</h1>
+      <Content part={part} exercises={exercises} />
+      <Total total={exercises} />
     </div>
   )
 }

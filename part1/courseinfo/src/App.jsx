ff6c8a59108474d04b63640b91f4b36a96ae63ae
jzshe <1165324684@qq.com>
2025-10-21 20:52:41 +0800

    加入按钮自增和清零功能

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,19 +3,24 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-
-  console.log('rendering...', counter)
+  const handleClick = () => {
+    console.log('clicked')
+    setCounter(counter + 1)
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={handleClick}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
   )
-}
 
+}
 export default App
 
 // 组件状态每次改变(setCounter) , 每次都会重新渲染 App ，从而再次触发 setTimeout ，形成循环。

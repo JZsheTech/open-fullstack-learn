4106caef5b55a08f58cae61d63504516a4dcccf7
jzshe <1165324684@qq.com>
2025-10-21 20:48:30 +0800

    log记录循环渲染新值的过程

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
-
 
   setTimeout(
     () => setCounter(counter + 1),
     1000
   )
+
+
+  console.log('rendering...', counter)
 
   return (
     <div>{counter}</div>
@@ -16,3 +17,5 @@ const App = () => {
 }
 
 export default App
+
+// 组件状态每次改变(setCounter) , 每次都会重新渲染 App ，从而再次触发 setTimeout ，形成循环。

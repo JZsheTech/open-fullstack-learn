473770af78c74118d2eced08de93a4b72eb8b467
jzshe <1165324684@qq.com>
2025-10-21 20:55:57 +0800

    标准的事件绑定写法和避免死循环的方法

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,13 +8,15 @@ const App = () => {
     setCounter(counter + 1)
   }
 
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
       <div>{counter}</div>
       <button onClick={handleClick}>
         plus
       </button>
-      <button onClick={() => setCounter(0)}>
+      <button onClick={setToZero}> 
         zero
       </button>
     </div>
@@ -24,3 +26,4 @@ const App = () => {
 export default App
 
 // 组件状态每次改变(setCounter) , 每次都会重新渲染 App ，从而再次触发 setTimeout ，形成循环。
+// 绑定的button中不能直接写 setCounter(0) , 会导致组件渲染时就执行函数，然后又进入下一次渲染，又执行函数，产生死循环。

bfd0341b97c6990ce943a7b4c44e3de386fb765c
jzshe <1165324684@qq.com>
2025-11-02 10:38:33 +0800

    Statistics统计组件拆分

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
+import Statistics from './Statistics'
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  // 计算统计信息
   const all = good + neutral + bad
   const average = all === 0 ? 0 : (good - bad) / all
   const positive = all === 0 ? 0 : (good / all) * 100
@@ -18,13 +18,14 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+      />
     </div>
   )
 }
